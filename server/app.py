fda7d85bf22652ba7aab49981eb5b3f698293fc1
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-07-29 11:38:37 +0300

    initial

--- server/app.py
+++ server/app.py
@@ -47,11 +47,48 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
 
+        if not username:
+            return {'message': 'Username is required'}, 400
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            session['user_id'] = user.id
+            return jsonify(user.to_dict()), 200
+        else:
+            return {'message': 'User not found'}, 404
+
+
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
+    
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return jsonify(user.to_dict()), 200
+            else:
+                return {'message': 'User not found'}, 404
+        else:
+            return {}, 401
+        
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
